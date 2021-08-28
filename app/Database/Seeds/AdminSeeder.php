05af0a7d5c459159b9ca3f960760e5eff4a13e66
imronpuji <imronpuji5@gmail.com>
2021-08-28 20:32:51 +0700

    add-fitur-upgrade-account

--- app/Database/Seeds/AdminSeeder.php
+++ app/Database/Seeds/AdminSeeder.php
@@ -5,13 +5,9 @@ class AdminSeeder extends \CodeIgniter\Database\Seeder
     public function run()
     {
         $data = [
-            'username'  => 'admin',
-            'name'      => 'Admin',
-            'email'     => 'admin@example.com',
-            'password'  => '$2y$10$sOKww3LkoNzBtuW5SKxcJOVcY5eN3L1UsTMZpzgWLDd5MfiZ2mmNe',
-            'status'    => 'Active',
-            'level'     => 'Admin'
+            'group_id'  => 3,
+            'user_id'      => 50,
         ];
-        $this->db->table('users')->insert($data);
+        $this->db->table('auth_groups_users')->insert($data);
     }
 } 
