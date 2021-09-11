92fff99e23d4d931e1aa67411d57209ac52f72ee
imronpuji <imronpuji5@gmail.com>
2021-09-11 14:17:32 +0700

    kjk

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -94,6 +94,14 @@
     </li>
     <?php endif; ?>
 
+      <?php if(in_groups(3)): ?>
+    <li class="menu-item">
+        <a class="menu-link" href="<?php base_url() ?>/request/wd"> <i class="icon material-icons md-monetization_on"></i>
+            <span class="text">Penarikan Dana</span>
+        </a>
+    </li>
+    <?php endif; ?>
+
     <?php if(in_groups(1)): ?>
     <li class="menu-item has-submenu">
         <a class="menu-link" href="page-orders-1.html"> <i class="icon material-icons md-shopping_cart"></i>
