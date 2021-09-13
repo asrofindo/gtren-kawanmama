f45dbfe724c4a5f3d295d305b566ee42d3e8b2a3
imronpuji <imronpuji5@gmail.com>
2021-09-13 21:24:46 +0700

    sd

--- app/Controllers/Order.php
+++ app/Controllers/Order.php
@@ -163,7 +163,7 @@ class Order extends BaseController
 		// data dari transaksi dan detail transaksi
 		$id_transaksi = $this->model->find($order_id)->id;
  
-		$data_transaksis = $this->detailtransaksi->select('detailtransaksi.id as id, cart_item.amount')->where('transaksi_id', $id_transaksi)
+		$data_transaksis = $this->detailtransaksi->select('detailtransaksi.id as id, cart_item.amount, detailtransaksi.status_barang  as status_barang')->where('transaksi_id', $id_transaksi)
 		->join('cart_item', 'cart_item.id = detailtransaksi.cart_id')
 		->findAll();
 		
