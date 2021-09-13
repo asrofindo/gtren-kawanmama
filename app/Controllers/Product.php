dff21c0e6bf5c3bf520320e8e89a835ed722a5ef
unknown <husein.18@students.amikom.ac.id>
2021-09-14 03:35:03 +0700

    husein

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -151,7 +151,7 @@ class Product extends BaseController
 			->join('distributor', 'distributor.user_id = users.id', 'left')
 			->join('product_distributor', 'product_distributor.distributor_id = distributor.id AND product_distributor.jumlah > 0', 'left')
 			->where('product_distributor.product_id', $product_id)->find();
-			dd($data['product_distributors'],'donk');
+			dd($data['product_distributor'],'donk');
 
 			$index = count($data['product_distributor']);
 			$data['product_distributors'] = [];
@@ -161,14 +161,15 @@ class Product extends BaseController
 				}
 			}
 
-			if ($data['product_distributors']==[]) {
+			if ($data['product_distributor']==[]) {
 				$data['product_distributor'] = $this->address->select('users.username,  kecamatan, kabupaten, kode_pos, provinsi, type, distributor.user_id, distributor.id as distributor_id, product_distributor.product_id, detail_alamat, locate')
 				->join('users', 'users.id = address.user_id', 'left')
 				->where('address.type', 'distributor')
 				->join('distributor', 'distributor.user_id = users.id', 'left')
 				->join('product_distributor', 'product_distributor.distributor_id = distributor.id AND product_distributor.jumlah > 0', 'left')
 				->where('product_distributor.product_id', $product_id)->find();
-			dd($data['product_distributors'],'cekan');
+
+				$data['product_distributors']=$data['product_distributor']
 			}
 
 			return view('commerce/product_detail', $data);
