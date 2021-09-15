5ae94b8eb60215faa940ea691d4a41b78e1bb918
imronpuji <imronpuji5@gmail.com>
2021-09-15 14:39:19 +0700

    imron

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -339,7 +339,7 @@ class Admin extends BaseController
 		$db->table('cart_item')->truncate();
 		$db->table('upgrades')->truncate();
 		
-		return '<a href="/">Berhasil, klik untuk kembali</>';
+		return redirect()->back();
 	}
 
 	public function admin_konfirmasi(){
