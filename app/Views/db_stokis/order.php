eb2eb04202b7b29a032fcdf0c2cccd7cec94f703
imronpuji <imronpuji5@gmail.com>
2021-09-16 00:59:41 +0700

    imron

--- app/Views/db_stokis/order.php
+++ app/Views/db_stokis/order.php
@@ -76,7 +76,9 @@
                                     <a href="#" data-bs-toggle="dropdown" class="btn btn-light rounded btn-sm font-sm"> <i class="material-icons md-more_horiz"></i> </a>
                                     <div class="dropdown-menu">
                                         <a class="dropdown-item" href="<?= base_url() ?>/order/detail/stockist/<?= $order->id; ?>">Detail</a>
-                                        <a class="dropdown-item btn-acc">Kirim Barang</a>
+                                        <?php if($order->status_barang == 'ditolak'): ?>
+                                            <a class="dropdown-item btn-acc">Kirim Barang</a>
+                                        <?php endif; ?>
 
                                     </div>
                                 </div> <!-- dropdown //end -->
