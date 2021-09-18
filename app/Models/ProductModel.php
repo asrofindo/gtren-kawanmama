c420f2ddffceb5f318069d6e27c3c81d9e0e362b
imronpuji <imronpuji5@gmail.com>
2021-09-18 14:46:32 +0700

    imrono

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -12,7 +12,7 @@ class ProductModel extends Model
 	protected $useAutoIncrement     = true;
 	protected $insertID             = 0;
 	protected $returnType           = 'App\Entities\Product';
-	protected $useSoftDeletes       = false;
+	protected $useSoftDeletes       = true;
 	protected $protectFields        = true;
 	protected $allowedFields        = [
 		'name', 'description', 'categories',
