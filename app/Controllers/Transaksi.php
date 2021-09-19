e7234d18e3b2e34e923bbe402615d6b591446703
imronpuji <imronpuji5@gmail.com>
2021-09-20 03:08:59 +0700

    imron

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -202,7 +202,7 @@ class Transaksi extends BaseController
 			"kode_unik" => $kode_unik, 
 			"bill_id" => $bill, 
 			"status_pembayaran" => $rekening != null ? "paid" : "pending", 
-			"total" => $total - $kode_unik, 
+			"total" => $total, 
 			"alamat" => $alamat]);
 		
 		foreach($data['carts'] as $cart){
