b72fd27952d970feda9f3360e01a5fb090e6ab5f
unknown <husein.18@students.amikom.ac.id>
2021-09-21 23:59:50 +0700

    chuseij

--- app/Views/commerce/home.php
+++ app/Views/commerce/home.php
@@ -32,7 +32,7 @@
                 <div class="row product-grid-4">
                     <!-- Start -->
                     <?php  foreach ($products as $product): ?>
-                    <div class="col-lg-3 col-md-4 col-12 col-sm-6">
+                    <div class="col-lg-3 col-md-4 col-6  col-sm-6">
                         <div class="product-cart-wrap mb-30">
                             <div class="product-img-action-wrap">
                                 <div class="product-img product-img-zoom">
