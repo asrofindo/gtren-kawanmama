64c8f6f972e8d2103c0d6e84269510a21ceefb3c
imronpuji <imronpuji5@gmail.com>
2021-09-23 19:19:19 +0700

    imron

--- app/Views/commerce/account.php
+++ app/Views/commerce/account.php
@@ -217,8 +217,7 @@
                                 <div class="tab-pane fade active show" id="orders" role="tabpanel" aria-labelledby="orders-tab">
                                     <div class="card">
                                         <div class="card-header">
-                                            <h5 class="mb-0">Saldo Anda</h5>
-                                            <h5 class="mb-0"><?= number_format($saldo); ?></h5>
+                                            <h5 class="mb-0">Saldo Anda : <?= number_format($saldo); ?></h5>
                                         </div>
                                             <form method="post" action="<?= base_url()?>/rekening">
                                                 <div class="row m-2">
