fbaf93313b4f16eee4c8fd58e8fa3ea8a881ae27
unknown <husein.18@students.amikom.ac.id>
2021-09-25 18:58:46 +0700

    husein

--- app/Views/commerce/templates/index.php
+++ app/Views/commerce/templates/index.php
@@ -185,7 +185,7 @@ h1 { font-size: 1.5em; margin: 10px; }
                 </div>
 
             <?php } ?>
-              <div class="d-flex flex-row mt-5"><input maxlength="1" name="valOne"  style="margin:8px" type="text" class="form-control input-class" autofocus=""><input maxlength="1" name="valTwo"  style="margin:8px" type="text" class="form-control input-class"><input maxlength="1" name="valTree"  style="margin:8px" type="text" class="form-control input-class"><input maxlength="1" name="valFour"  style="margin:8px" type="text" class="form-control input-class"><input maxlength="1" name="valFive"  style="margin:8px" type="text" class="form-control input-class"></div>
+              <div class="d-flex flex-row mt-5"><input maxlength="1" name="valOne"  style="margin:8px" type="number" class="form-control input-class" autofocus=""><input maxlength="1" name="valTwo"  style="margin:8px" type="number" class="form-control input-class"><input maxlength="1" name="valTree"  style="margin:8px" type="number" class="form-control input-class"><input maxlength="1" name="valFour"  style="margin:8px" type="number" class="form-control input-class"><input maxlength="1" name="valFive"  style="margin:8px" type="number" class="form-control input-class"></div>
             </div>
             <div class="modal-footer">
               <a href="<?php base_url() ?>/verifywa/<?= user()->id ?>">Klik Di sini Untuk Minta Kode OTP Baru</a>
