a58ee137c85c2f5fe761fd140fdfff22e7d15ee6
imronpuji <imronpuji5@gmail.com>
2021-09-28 14:43:37 +0700

    imron

--- app/Views/commerce/home.php
+++ app/Views/commerce/home.php
@@ -115,8 +115,8 @@
                                 <div class="product-img product-img-zoom">
                                     <a href="<?= base_url('product/'. $product->slug)?>">
                                     <?php for($i = 0; $i < 2; $i++): ?>
-                                            <img class="<?= $i == 0 ? 'default-img' : 'hover-img'  ?>" src="<?= base_url() ?>/public/uploads/product_photos/<?= $product->photos[$i]; ?>" alt="">
-                                            <?php endfor ?>
+                                        <img class="<?= $i == 0 ? 'default-img' : 'hover-img'  ?>" src="<?= base_url() ?>/public/uploads/product_photos/<?= $product->photos[$i]; ?>" alt="">
+                                    <?php endfor ?>
                                     </a>
                                 </div>
                                 <div class="product-badges product-badges-position product-badges-mrg">
