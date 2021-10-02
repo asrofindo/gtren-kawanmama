5f58c3e2f6c8ae5df40ae6f14f3c6a2856229291
Asrofi <mail@asrofi.web.id>
2021-10-02 12:56:55 +0700

    asrofi rubah app/config/app.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
 	 * @var string
 	 */
 
-	public $baseURL = 'https://trenbisnis.net';
+	public $baseURL = 'https://g.trenbisnis.net';
 
 	/**
 	 * --------------------------------------------------------------------------
