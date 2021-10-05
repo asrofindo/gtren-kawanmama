59a54e2e559cb895e31d2c7b87d5ece854b25142
imronpuji <imronpuji5@gmail.com>
2021-10-05 13:17:27 +0700

    imron

--- app/Views/db_admin/produk/tambah_produk.php
+++ app/Views/db_admin/produk/tambah_produk.php
@@ -178,7 +178,7 @@
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/codemirror.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/mode/xml/xml.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/2.36.0/formatting.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js" defer></script>
 <script type="text/javascript">
    $(document).ready(function() {
   $('#summernote').summernote();
