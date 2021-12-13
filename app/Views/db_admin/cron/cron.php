448e6c1a40d9e533fc24b75263b96126b49fa181
imronpuji <imronpuji5@gmail.com>
2021-12-13 23:46:58 +0700

    imron

--- app/Views/db_admin/cron/cron.php
+++ app/Views/db_admin/cron/cron.php
@@ -9,7 +9,13 @@
         <div class="row m-4">
             <ul>
                 <li>Anda Harus Registrasi Terlebih Dahulu ke Link Berikut <a href="https://console.cron-job.org/login">Cron Job</a></li>
-                <li>Pasang Link Berikut <a href="" id="url"></a></li>
+            </ul>
+
+            <p>Terdapat dua Jenis Cron job</p>
+            <ul>
+                <li><a href="" id="url"></a>, digunakan ketika ada transaksi yang belum selesai di karenakan pembeli belum menyatakan menerima barang setelah 10 hari sejak barang dikirim, cron tersebut akan merubah status dari transaksi delivered tersebut menjadi sedang di pantau oleh admin</li>
+
+                <li><a href="" id="urls"></a>, digunakan ketika ada transaksi yang belum diterima oleh seller sejak pesanan disetujui oleh admin, cron tersebut akan merubah status yang sebelumnya pending menjadi ditolak</li>
             </ul>
         </div>
         <br>
@@ -19,6 +25,7 @@
 
 <script>
    $('#url').text(window.location.protocol + '//' + window.location.host + '/cron')
+   $('#urls').text(window.location.protocol + '//' + window.location.host + '/wacron')
 </script>
 
 <?php $this->endSection() ?>
