52261a18fa0ec1c06c4f08ab26e279a7a18f5b72
imronpuji <imronpuji5@gmail.com>
2021-12-13 15:33:24 +0700

    errorDashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -3,14 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <?php if(in_groups(1)) {?>
-        <title>Gtren dashboard Admin</title>
-    <?php }elseif(in_groups(3)) {?>
-        <title>Gtren dashboard Distributor</title>
-    <?php }elseif(in_groups(4)) {?>
-        <title>Gtren dashboard Affiliate</title>
-    <?php }?>
-
+    <title><?= profile() == null ? 'suply chain' : profile()->title; ?></title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
