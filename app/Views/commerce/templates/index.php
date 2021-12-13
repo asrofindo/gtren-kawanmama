ef466587428320ad388d6621a2ab7e72b95c761c
imronpuji <imronpuji5@gmail.com>
2021-12-13 10:38:23 +0700

    fixErrorHome

--- app/Views/commerce/templates/index.php
+++ app/Views/commerce/templates/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php if(isset($title)){echo $title;}else{echo 'Gtren';}?></title>
+    <title><?= profile() == null : 'supply chain' : profile()->title; ?></title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,7 +12,7 @@
     <meta property="og:url" content="">
     <meta property="og:image" content="">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>/uploads/banner/<?= profile()->favicon ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>/uploads/banner/<?= profile() == null ? '' : profile()->favicon ?>">
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="<?= base_url()  ?>/frontend/css/main.css">
